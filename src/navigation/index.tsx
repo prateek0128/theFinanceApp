f5657b53b2ea3ba0bd682e7972011f293a4bd4cb
prateek0128 <rajput.prateek28@gmail.com>
2025-05-20 16:22:22 +0530

    Second commit-Splash

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,13 +5,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "../screens/auth/login/login";
 import SignUpScreen from "../screens/auth/signUp/signUp";
 import HomeScreen from "../screens/homePage/homePage";
+import SplashScreen from "../screens/splashScreen/splashScreen";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUp">
+      <Stack.Navigator
+        initialRouteName="Splash"
+        //screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
