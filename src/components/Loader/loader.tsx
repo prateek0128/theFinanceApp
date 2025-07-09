ec23d56feffe380ee57333374aaf58a651a33a2e
princesharma2108 <sharma.prince2108@gmail.com>
2025-07-09 11:25:48 +0530

    edit message pop up and convert choose intrest into dark theme

--- src/components/Loader/loader.tsx
+++ src/components/Loader/loader.tsx
@@ -2,12 +2,27 @@
 import React from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { colors } from "../../assets/styles/colors";
+import { useContext } from "react";
+import { ThemeContext } from "../../context/themeContext";
 
-const Loader = () => (
-  <View style={styles.center}>
-    <ActivityIndicator size="large" color={colors.primary} />
-  </View>
-);
+const Loader = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  return (
+    <View
+      style={[
+        styles.center,
+        {
+          backgroundColor:
+            theme === "dark"
+              ? colors.darkPrimaryBackground
+              : colors.primaryBackground,
+        },
+      ]}
+    >
+      <ActivityIndicator size="large" color={colors.primary} />
+    </View>
+  );
+};
 
 export default Loader;
 
